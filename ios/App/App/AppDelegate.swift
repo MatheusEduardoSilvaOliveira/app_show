3cc37026ae90b48cfb421da829269ef41d01f422
MatheusEduardo <45131659+MatheusEduardoSilvaOliveira@users.noreply.github.com>
2020-10-13 17:12:47 -0400

    Icons Atualizados
    Funcionando IOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -9,7 +9,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-	FirebaseApp.configure()
+    FirebaseApp.configure()
     // Override point for customization after application launch.
 	  UNUserNotificationCenter.current().delegate = self
 
@@ -83,5 +83,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
-extension AppDelegate: UNUserNotificationCenterDelegate
-
+extension AppDelegate:UNUserNotificationCenterDelegate{}
